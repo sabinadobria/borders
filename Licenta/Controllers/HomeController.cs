daef57d13877ccdfbd8424fa97e888253438ab8b
vio <sabinadobria@gmail.com>
2018-04-24 10:48:06 +0300

    vio test commit

--- Licenta/Controllers/HomeController.cs
+++ Licenta/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace Licenta.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "test";
 
             return View();
         }
