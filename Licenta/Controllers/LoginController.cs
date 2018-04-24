0aad29b366e67cab18aa86a8565db2148d8e5c74
vio <sabinadobria@gmail.com>
2018-04-24 11:17:27 +0300

    LogIn Controller + - log in view - register view - forgot password view

---
+++ Licenta/Controllers/LoginController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Licenta.Controllers
+{
+    public class LoginController : Controller
+    {
+        public ActionResult LogIn()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        public ActionResult ForgotPassword()
+        {
+            return View();
+        }
+    }
+}
