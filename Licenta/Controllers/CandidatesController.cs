64b0ee90715f3f9a6834d5d5719351b54dd88fca
vio <sabinadobria@gmail.com>
2018-07-02 17:11:10 +0300

    incercare

--- Licenta/Controllers/CandidatesController.cs
+++ Licenta/Controllers/CandidatesController.cs
@@ -16,8 +16,39 @@ namespace Licenta.Controllers
         NoBordersDB db = new NoBordersDB();
         public ActionResult CandidatesList()
         {
+            try
+            {
+                List<CandidateProfile> candidateProfiles = db.CandidateProfiles.ToList();
+
+                List<CandidateExperience> candidateExperiences = db.CandidateExperiences.ToList();
+
+                List<CandidateStudies> candidateStudies = db.CandidateStudies.ToList();
+
+                List<CandidateTechnologies> candidateTechnologies = db.CandidateTechnologies.ToList();
+
+                List<CandidateLanguages> candidateLanguages = db.CandidateLanguages.ToList();
+
+                foreach (var candidate in candidateProfiles)
+                {
+
+                    candidate.CandidateExperience = candidateExperiences.Where(x => x.Id_candidate == candidate.Id_candidate).ToList();
+                    candidate.CandidateStudies = candidateStudies.Where(x => x.id_candidate == candidate.Id_candidate).ToList();
+                    candidate.CandidateLanguages = candidateLanguages.Where(x => x.id_candidate == candidate.Id_candidate).ToList();
+                    candidate.CandidateTechnologies = candidateTechnologies.Where(x => x.id_candidate == candidate.Id_candidate).ToList();
+                }
             
-            return View(db.CandidateProfiles.ToList());
+                
+                //return View(result);
+                //if (result.tech_level)
+              
+
+                return View(candidateProfiles);
+            }
+            catch
+            {
+                return View();
+            }
+         
         }
 
         public ActionResult Preview(int? id)
